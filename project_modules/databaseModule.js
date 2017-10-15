d2a3c7227e87bc07949b34cb48b7054fa2367a88
e1601515 <32124119+e1601515@users.noreply.github.com>
2017-10-15 11:18:46 +0300

    proto1

--- project_modules/databaseModule.js
+++ project_modules/databaseModule.js
@@ -1,3 +1,4 @@
+var debug = true;
 var sqlite3 = require('sqlite3').verbose();
 
 var transactionDatabaseFilepath = "./database/transactionTrackingDB.db"
--- project_modules/ethereumModule.js
+++ project_modules/ethereumModule.js
@@ -1,50 +1,59 @@
+var debug = true;
 var Web3 = require('web3');
-var util = require('ethereumjs-util');
+//var util = require('ethereumjs-util');
 var tx = require('ethereumjs-tx');
 var lightwallet = require('eth-lightwallet');
 var databaseModule = require('./databaseModule.js');
 var txHash;
 var web3 = new Web3();
+
 var saveTransaction = function(privateKey,fromAccount,toAccount,encryptedDataToSave,packetIdFromClient,companyNameFromClient)
 {
-  var txutils = lightwallet.txutils;
-  var hexPrivateKey = new Buffer(privateKey, 'hex');
-  var rawTx = {
-  from: fromAccount,
-  to: toAccount,
-  value: web3.toHex(0),
-  //this methdod fetches the integer that represents the count of transactions.
-  //if we knew this already, we could create transactions without our Ethereum node being fully synced.
-  nonce: web3.toHex(web3.eth.getTransactionCount(fromAccount)),
-  gasLimit: web3.toHex(800000),
-  gasPrice: web3.toHex(20000000000),
-  data: web3.toHex(encryptedDataToSave)
+  var rawTx =
+  {
+    from: fromAccount,
+    to: toAccount,
+    value: web3.toHex(0),
+    //this methdod fetches the integer that represents the count of transactions.
+    //if we knew this already, we could create transactions without our Ethereum node being fully synced.
+    nonce: web3.toHex(web3.eth.getTransactionCount(fromAccount)),
+    gasLimit: web3.toHex(800000),
+    gasPrice: web3.toHex(20000000000),
+    data: web3.toHex(encryptedDataToSave)
   };
+  //var txutils = lightwallet.txutils;
   var transaction = new tx(rawTx);
+  var hexPrivateKey = new Buffer(privateKey, 'hex');
   transaction.sign(hexPrivateKey);
   var serializedTx = transaction.serialize().toString('hex');
   web3.eth.sendRawTransaction(
-  '0x' + serializedTx, function(err, result) {
-      if(err) {
+  '0x' + serializedTx, function(err, result)
+  {
+      if(err)
+      {
         console.log(err);
-      } else {
-        console.log("No error. Transaction made with identifier: "+result);
+      }
+      else
+      {
+        console.log("Transaction made with identifier: "+result);
         txHash=result;
         databaseModule.saveToDB(packetIdFromClient,result);
       }
   });
 }
-var getFromEthereumFunction = function(searchTerm)
-{
-  console.log("search "+searchTerm);
-  var transactionFromEthereum = web3.toAscii(web3.eth.getTransaction(searchTerm).input);
-  console.log("ascii "+transactionFromEthereum);
-  return transactionFromEthereum;
-}
+
 var getLatest = function()
 {
   return txHash;
 }
+
+var getFromEthereumFunction = function(searchTerm)
+{
+  var transactionFromEthereum = web3.toAscii(web3.eth.getTransaction(searchTerm).input);
+  return transactionFromEthereum;
+}
+
+
 function connectEthereum()
 {
   web3.setProvider(new web3.providers.HttpProvider("http://localhost:8545"));
@@ -67,6 +76,7 @@ function connectEthereum()
     }
   }
 }
+
 exports.getFromEthereumFunction=getFromEthereumFunction;
 exports.saveTransaction=saveTransaction;
 exports.getLatest=getLatest;
